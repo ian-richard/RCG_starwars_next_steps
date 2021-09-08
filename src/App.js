42ac50a7a1f32591a744fb0e16b04e9febba1190
Ian F <ian.fiddeman@gmail.com>
2021-09-08 11:55:21 +0100

    post movie now working and get data displaying movies

--- src/App.js
+++ src/App.js
@@ -20,15 +20,18 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,11 +42,16 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    fetch('https://rcg-react-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://rcg-react-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
       method: 'POST',
-      body: JSON.stringify(movie)
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
+    const data = await response.json();
+    console.log(data)
   }
 
   let content = <p>Found no movies.</p>;
